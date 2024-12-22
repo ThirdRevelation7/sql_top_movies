4eb701245f6b39d96630816810ed9d54bf2355d5
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2024-12-22 08:09:05 -0500

    Add requirements.txt file for dependencies

--- main.py
+++ main.py
@@ -76,9 +76,9 @@ def home():
     result = db.session.execute(db.select(Movie).order_by(asc(Movie.rating)))
     all_movies = result.scalars().all()
 
-    # for i in range(len(all_movies)):
-    #     all_movies[i].ranking = len(all_movies) - i
-    # db.session.commit()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
 
     return render_template("index.html", movies=all_movies)
 
